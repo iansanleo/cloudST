40e77fae7b51274fffd8677c14e2b3f549cc6a55
saitohfin <himurafin@gmail.com>
2017-08-16 17:54:36 +0200

    Use SpringBootApplication

---
+++ src/main/java/com/cloudST/CloudSTApplication.java
@@ -0,0 +1,11 @@
+package com.cloudST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSTApplication {
+    public static void main(String[] args){
+        SpringApplication.run(CloudSTApplication.class, args);
+    }
+}
