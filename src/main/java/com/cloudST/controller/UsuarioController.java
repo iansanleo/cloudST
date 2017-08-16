a94a8f4e7f48f1b823bea7f306e6d4492697c606
saitohfin <himurafin@gmail.com>
2017-08-17 01:22:38 +0200

    Necessary things to load a jsp

--- src/main/java/com/cloudST/controller/UsuarioController.java
+++ src/main/java/com/cloudST/controller/UsuarioController.java
@@ -2,18 +2,27 @@ package com.cloudST.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.cloudST.mapper.UsuarioMapper;
 import com.cloudST.model.Usuario;
+
+import java.util.Map;
 
 @Controller
 public class UsuarioController {
 
-	private
-	UsuarioMapper usuarioMapper;
-	
+	//private UsuarioMapper usuarioMapper;
+
+
+	@GetMapping("/user")
+	public String usuario(Model model){
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Pepe");
+		model.addAttribute("name",usuario.getNombre());
+		return "usuario";
+	}
 	
 	//post solo formularios, get para todo
 	@RequestMapping(value="/user", method = RequestMethod.POST)
