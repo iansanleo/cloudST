b70cd9cc1060b76d50874da55b142bc61eb1a77c
Christian Sanchez <iansanleo@gmx.es>
2017-08-26 15:27:46 +1000

    Problem UserController

--- src/main/java/com/cloudST/controller/UsuarioController.java
+++ src/main/java/com/cloudST/controller/UsuarioController.java
@@ -1,3 +1,20 @@
+package com.cloudST.controller;
+ 
+import com.cloudST.model.Usuario;
+ 
+import com.cloudST.repository.UsuarioRepository;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+ 
+import org.springframework.stereotype.Controller;
+ 
+import org.springframework.ui.Model;
+ 
+import org.springframework.web.bind.annotation.GetMapping;
+ 
+import org.springframework.web.bind.annotation.PostMapping;
+ 
+
 @Controller
 public class UsuarioController {
 
